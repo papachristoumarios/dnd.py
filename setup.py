5ed55ce67f1d0d10c731f0ee83b267bae6215a9f
Marios Papachristou <papachristoumarios@gmail.com>
2019-04-08 22:10:49 +0300

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dnd.py',
+    version='1.0',
+    description='Do not distract yourself!',
+    author='Marios Papachristou',
+    author_email='papachristoumarios@gmail.com',
+    url='https://github.com/papachristoumarios/dnd.py',
+    scripts=['dnd.py']
+)
